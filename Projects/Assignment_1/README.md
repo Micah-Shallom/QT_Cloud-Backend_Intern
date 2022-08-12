05c27dfaa35b14e6b85ddb01739c8efe5118f590
Micah Shallom <micahshallom@gmail.com>
2022-08-12 11:54:25 +0100

    submission

---
+++ Projects/Assignment_1/README.md
@@ -0,0 +1,2 @@
+## Create a program that shows your email adress & name
+## Create a program that shows your Stack track 
---
+++ Projects/Assignment_1/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+func main(){
+	
+	fmt.Println("Email: micahshallom@gmail.com")
+	fmt.Println("Name: Shallom Micah Bawa")
+	fmt.Println("Stack: Backend/Cloud Developer")
+}
